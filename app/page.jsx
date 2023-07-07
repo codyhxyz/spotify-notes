50ffbbd567d08d73930648c107e33f3845fe7705
ydoc5212 <chergenroeder135@gmail.com>
2023-07-06 19:20:17 -0700

    update vercel url

--- app/page.jsx
+++ app/page.jsx
@@ -4,17 +4,18 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 // supabase auth tied to spotify provider auth
 export default function Home() {
   const supabase = createClientComponentClient()
-  const REDIRECT_URL = 'spotify-notes.vercel.app/home'
+  const REDIRECT_URL = 'https://spotify-notes.vercel.app/home'
 
   async function spotifyLogin(sb) {
     console.log('authorizing user via supabase + spotify...')
-    await sb.auth.signInWithOAuth({
+    const {data} = await sb.auth.signInWithOAuth({
       provider: 'spotify',
       options: {
         scopes: 'user-read-email, user-modify-playback-state, user-read-playback-state, user-read-recently-played',
         redirectTo: {REDIRECT_URL}
       }
     })
+
   }
   //redirects user to /home
   spotifyLogin(supabase)
