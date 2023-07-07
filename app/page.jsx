f5fcbb6d9340c7ab01f7abf0521dbdf09e01b097
ydoc5212 <chergenroeder135@gmail.com>
2023-07-06 20:24:44 -0700

    update redirect url vercel

--- app/page.jsx
+++ app/page.jsx
@@ -6,20 +6,19 @@ export default function Home() {
   const supabase = createClientComponentClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY)
   const REDIRECT_URL = 'https://my-spotify-notes.vercel.app'
 
-  async function spotifyLogin(sb) {
-    console.log('authorizing user via supabase + spotify...')
-    const {data} = await sb.auth.signInWithOAuth({
+  //redirects user to /home
+  
+  console.log('authorizing user via supabase + spotify...')
+  const spotifyLogin = async() => {
+    const {data} = await supabase.auth.signInWithOAuth({
       provider: 'spotify',
       options: {
         scopes: 'user-read-email, user-modify-playback-state, user-read-playback-state, user-read-recently-played',
-        redirectTo: {REDIRECT_URL}
+        redirectTo: REDIRECT_URL
       }
     })
-
-
   }
-  //redirects user to /home
-  spotifyLogin(supabase)
+  spotifyLogin() 
   
   return (
     <main>
