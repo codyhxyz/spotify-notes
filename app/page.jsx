167351fa582985cc60d3ab19eb6918cc088b09d6
ydoc5212 <chergenroeder135@gmail.com>
2023-07-09 13:52:50 -0700

    only call spotifyLogin() client-side

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-"use client"; //what does this do
+"use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect } from "react";
 
@@ -8,21 +8,28 @@ export default function Home() {
 
   console.log("authorizing user via supabase + spotify...");
   async function spotifyLogin() {
-    // set dynamic redirect URL for functionality across dev and prod environemnts
-    // REDIRECT_URL should match one of Supabase 'Site URL' and 'Redirect URLs' in Auth/ URL Config
-    const REDIRECT_URL = window.location.origin;
+    // check if window object is available
+    if (typeof window !== undefined) {
+      // set dynamic redirect URL for functionality across dev and prod environemnts
+      // REDIRECT_URL should match one of Supabase 'Site URL' and 'Redirect URLs' in Auth/ URL Config
+      const REDIRECT_URL = window.location.origin;
 
-    console.log("authorizing user via supabase + spotify...");
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "spotify",
-      options: {
-        scopes:
-          "user-read-email, user-modify-playback-state, user-read-playback-state, user-read-recently-played",
-        redirectTo: REDIRECT_URL,
-      },
-    });
+      console.log("authorizing user via supabase + spotify...");
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "spotify",
+        options: {
+          scopes:
+            "user-read-email, user-modify-playback-state, user-read-playback-state, user-read-recently-played",
+          redirectTo: REDIRECT_URL,
+        },
+      });
+    }
   }
-  spotifyLogin();
+
+  // useEffect ensures that spotifyLogin() is only called client-side
+  useEffect(() => {
+    spotifyLogin();
+  }, []);
 
   return (
     <main>
