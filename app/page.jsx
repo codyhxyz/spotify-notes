2846df7d30f3c4d331f6d30875b074fd50007d08
ydoc5212 <chergenroeder135@gmail.com>
2023-07-09 13:59:21 -0700

    fix redirect URL

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     if (typeof window !== undefined) {
       // set dynamic redirect URL for functionality across dev and prod environemnts
       // REDIRECT_URL should match one of Supabase 'Site URL' and 'Redirect URLs' in Auth/ URL Config
-      const REDIRECT_URL = window.location.origin;
+      const REDIRECT_URL = window.location.origin + "/home";
 
       console.log("authorizing user via supabase + spotify...");
       const { data, error } = await supabase.auth.signInWithOAuth({
