650ce99e10da492220fa2fa323087b93dd8006b8
ydoc5212 <chergenroeder135@gmail.com>
2023-07-10 02:02:25 -0700

    formatting changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { spotifyLogin } from "../util/authutils";
-import Image from "next/image";
 
 // supabase auth tied to spotify provider auth
 export default function Home() {
@@ -21,10 +20,18 @@ export default function Home() {
           >
             Log In With Spotify
           </button>
-          <p>
-            Please note that full navigation features are only possible on{" "}
+          <p className="text-message">
+            Quick Start: Use Spotify to play songs, and the app will follow
+            along!
+          </p>
+          <p className="text-message sent">
+            Please Note: Full navigation features are only possible on{" "}
             <b>Spotify Premium</b> accounts.
           </p>
+          {/* <p className="login-p">
+            Please Note: Full navigation features are only possible on{" "}
+            <b>Spotify Premium</b> accounts.
+          </p> */}
         </div>
         <div>
           <img id="login-artwork" src="/icon.ico" alt="" />
